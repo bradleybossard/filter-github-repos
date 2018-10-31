6aeec6295f6ebe66e97be858147cbf750d09bdcb
Bradley Bossard <bradleybossard@gmail.com>
2018-10-31 17:26:29 -0500

    Add logic for caching response

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
+const fs = require('fs');
 const request = require('request-promise-native');
 const _ = require('lodash');
 
 const username = 'bradleybossard';
 const url = `https://api.github.com/users/${username}/repos`;
+const reposFilename = './repos.json';
 
-var options = {
-  url,
-  headers: {
-    'User-Agent': 'request'
-  }
-};
+function fetchData() {
+  const options = {
+    url,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
-request.get(options)
-.then(res => {
-  console.log(res);
-});
+  return request.get(options)
+  .then(res => {
+    console.log(`File written with ${res.length} entries`);
+    fs.writeFile(reposFilename, res);
+    return Promise.resolve();
+  });
+}
+
+if (!fs.existsSync(reposFilename)) {
+  fetchData();
+}
